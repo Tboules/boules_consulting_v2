ddd01d50486b8f9d4ab44c3d4547fcfc76201818
Tony Boules <tboules@gmail.com>
2021-12-28 12:40:27 -0800

    gtag v2

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingIds: ["UA-135218299-1", "AW-10832162944"],
+        trackingIds: ["UA-216023760-1"],
         gtagConfig: {
           optimize_id: "OPT_CONTAINER_ID",
           anonymize_ip: true,
